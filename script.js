40c247b55c17d3fe2e4eea86caaf8a41433f38f0
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-03-18 23:43:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function getData() {
-    const response = await fetch('https://apicc.42web.io/proxy.php?url=https://oneuser.com/data.json');
+    const response = await fetch('https://apicc.42web.io/proxy.php?url=https://blbms.github.io/oneuser/data.json');
     if (!response.ok) {
         console.error("Napaka pri pridobivanju podatkov");
         return;
